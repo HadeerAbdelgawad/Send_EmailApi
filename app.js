0591fec092c439d39e0c7d5cc7a427e5ac4369ec
hadeer <hadeer.abelgawad44@gmail.com>
2025-05-19 18:49:31 +0300

    'loacl'

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { connectToDatabase } = require('./src/db/mongoose');
 // CORS configuration for security
 const cors = require('cors');
 const corsOptions = {
-    origin: ['http://localhost:3000','http://localhost:5175/ksa-afflite/', 'https://your-frontend-domain.com', '*'], // Add your frontend domains and allow all origins during development
+    origin: ['http://localhost:3000','http://localhost:5176/ksa-afflite/', 'https://your-frontend-domain.com', '*'], // Add your frontend domains and allow all origins during development
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type', 'Authorization']
 };
